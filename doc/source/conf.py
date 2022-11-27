079bff6608c9bc04ce5b5af55089fb738739fe09
AntoineMdn <antoine.mandin.am@gmail.com>
2022-11-27 14:09:31 +0100

    :memo: Update doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,30 +1,26 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import datetime
 
-project = 'Sphinx mermaid'
-copyright = '2022, Antoine Mandin'
-author = 'Antoine Mandin'
-release = '0.0.1'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Project values
+project = "Sphinx mermaid"
+author = "Antoine Mandin"
+copyright = f"{datetime.date.today().strftime('%Y')}, Antoine Mandin"
+release = "0.0.2"
 
 extensions = [
-    'sphinxmermaid'
+    "sphinxmermaid", # Mermaid extension
+    "sphinx_rtd_theme", # Read the docs them
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+# Use Read the docs theme
+html_theme = "sphinx_rtd_theme"
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# Link to Github repo
+html_context = {
+    "display_github": True,
+    "github_user": "Doreapp",
+    "github_repo": "sphinx-mermaid",
+    "github_version": "main",
+    "conf_py_path": "/doc/source/",
+}
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -1,27 +1,19 @@
-.. Sphinx mermaid documentation master file, created by
-   sphinx-quickstart on Sat Nov 26 17:35:15 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to Sphinx mermaid's documentation!
+Mermaid in sphinx documentation
 ==========================================
 
-.. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+Quick example
+-------------
 
+.. code::
 
+   .. mermaid::
 
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
-
+      graph TD
+      A --> B
+      B -- With a label --> C
 
 .. mermaid::
 
    graph TD
-
    A --> B
+   B -- With a label --> C
